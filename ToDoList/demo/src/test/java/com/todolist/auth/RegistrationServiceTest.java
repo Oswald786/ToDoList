a017fb32856f4b57d1dd261e7aa7a3c87f71ab49
Oswald786 <ethanslade63@gmail.com>
2025-11-02 15:19:43 +0000

    JWT token works it will now generate a token properly working on getting autenticationfollwoed by testing and clean up

---
+++ ToDoList/demo/src/test/java/com/todolist/auth/RegistrationServiceTest.java
@@ -0,0 +1,12 @@
+package com.todolist.auth;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationServiceTest {
+
+    @Test
+    void register() {
+    }
+}
